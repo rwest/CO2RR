43a0ac6af438d135ce482c8e55ea0a2027e012a9
Richard West <r.west@northeastern.edu>
2025-12-05 13:59:04 -0500

    Preparing for GSA.
    So far, just merged cells into a function.

--- AIChE_2025/CO2RR_RMS_Diffusion_GSA.jl
+++ AIChE_2025/CO2RR_RMS_Diffusion_GSA.jl
@@ -15,7 +15,7 @@
 # ---
 
 # %% [markdown]
-# # Simulation with Diffusion Layer
+# # Simulation with Diffusion Layer, and Global Sensitivity Analysis
 #
 
 # %%
@@ -29,6 +29,10 @@ using Sundials
 using SciMLBase
 using QuadGK
 using CSV, DataFrames
+
+using GlobalSensitivity
+using Random
+using Statistics
 
 # %%
 using ReactionMechanismSimulator
@@ -45,94 +49,93 @@ interfacerxns = outdict[Set(["surface", "gas"])]["Reactions"]
 solv = outdict["Solvents"][1];
 
 # %%
-sitedensity = 2.294e-5; # Ag111 site density is 2.294e-9 mol/cm^2 or 2.294e-5 mol/m^2
-boundarylayer = IdealDiluteSolution(boundarylayerspcs,boundarylayerrxns,solv,name="boundarylayeruid",diffusionlimited=true);
-surf = IdealSurface(surfspcs,surfrxns,sitedensity,name="surface");
+function run_co2_reduction_simulation(params::Vector{Float64})
+    sitedensity = 2.294e-5; # Ag111 site density is 2.294e-9 mol/cm^2 or 2.294e-5 mol/m^2
+    phi = -(1.37 + 0.414);
+
+    # Reservoir is a 100 mL (100e-6 m^3) cell
+    # Proton concentration is 10^-7 mol/L (10^-4 mol/m^3)
+    # CO2 concentration is 0.01 mol/L (10 mol/m^3), saturation solubility ~0.03 mol/L
+    # AVratio in experiments is 36 m^-1 but is measured by surface area/reservoir volume
+    # Area of the electrode is therefore 3.6e1 m^-1 * 1e2 * 1e-6 m^3 = 3.6e-3 m^2 = 36 cm^2
+    # Assume boundary layer thickness d_bl = 1 mm or 1e-3 m
+    # Volume of the boundary layer V_bl = 3.6e-3 m^2 * 1e-3 m = 3.6e-6 m^3
+    # Actual AVratio is therefore 3.6e-3 m^2 / 3.6e-6 m^3 = 1e3 m^-1 (reciprocal of d_bl)
+    # Amount of sites is 2.943e-5 mol/m^2 * 3.6e-3 m^2 = 10.595e-8 mol
+
+    # For earlier simulations, a 100x linear scale factor is applied,
+    # so volume becomes 100e-6 m^3 * (1e2)^3 = 100 m^3,
+    # electrode area becomes 3.6e-3 * (1e2) ^2 = 3.6e1 m^2,
+    # AVratio becomes 3.6e1 m^2 / 1e2 m^3 = 0.36 m^-1
+    # Volume of the boundary layer becomes 3.6e1 m^2 * 1e-3 m = 3.6e-2 m^3
+
+    C_proton = 1e-7*1e3;
+    C_co2 = 1e-2*1e3;
+    C_default = 1e-12;
+    V_res = 1e3;
+    layer_thickness = 1e-5;
+    AVratio = 36;
+    A_surf = V_res*AVratio;
+    V_bl = A_surf*layer_thickness;
+    # V_bl = V_res;
+    sites = sitedensity*A_surf;
+    # Set proton diffusivity to a higher value than calculated from Stokes Einstein equation
+    # The values are taken from DOI: 10.1039/C8SC01253A
+    # Values calculated from MD is 1.015 A^2/ps, experimental values are 0.932 A^2/ps.
+    # 1 A^2/ps = 1e-8 m^2/s
+    proton_diffusivity = 0.932e-8 # m^2/s
+
+    boundarylayer = IdealDiluteSolution(boundarylayerspcs,boundarylayerrxns,solv,name="boundarylayeruid",diffusionlimited=true);
+    surf = IdealSurface(surfspcs,surfrxns,sitedensity,name="surface");
+
+    # The initial conditions for individual species are moles not concentration, so we need to multiply concentration by boundary layer volume
+    initialcondsboundarylayer = Dict(["proton"=>C_proton*V_bl,
+                                    "CO2"=>C_co2*V_bl,
+                                    #   "H2"=>C_default*10*V_bl,
+                                    #   "O=CO"=>C_default*V_bl,
+                                    "V"=>V_bl,"T"=>300,"Phi"=>0.0,"d"=>0.0]);
+    initialcondsreservoir = Dict(["proton"=>C_proton,
+                                "CO2"=>C_co2,
+                                "V"=>V_res,"T"=>300]);
+
+
+    # Assume voltage is -1.0 V vs. R.H.E. which equates to -1.414 V vs. S.H.E. at pH=7
+    initialcondssurf = Dict(["CO2X"=>0.4*sites,
+            # "CHO2X"=>0.1*sites,
+            # "CO2HX"=>0.1*sites,
+            # "OX"=>0.1*sites,
+            # "OCX"=>0.1*sites,
+            "vacantX"=>0.6*sites,
+            # "CH2O2X"=>0.05*sites,
+            # "CHOX"=>0.04*sites,
+            # "CH2OX"=>0.01*sites,
+            "A"=>A_surf,"T"=>300,"Phi"=>phi]);
+
+    domainboundarylayer, y0boundarylayer, pboundarylayer = ConstantTVDomain(phase=boundarylayer, initialconds=initialcondsboundarylayer);
+    domaincat,y0cat,pcat = ConstantTAPhiDomain(phase=surf, initialconds=initialcondssurf);
+
+
+    domainboundarylayer.diffusivity[6] = proton_diffusivity
+
+    inter,pinter = ReactiveInternalInterfaceConstantTPhi(domainboundarylayer,
+        domaincat,interfacerxns,298.15,A_surf);
+
+    # start with 1mm layer thickness
+    diffusionlayer = ConstantReservoirDiffusion(domainboundarylayer, initialcondsreservoir, A_surf, layer_thickness);
+    interfaces = [inter, diffusionlayer];
+
+    @time react,y0,p = Reactor((domainboundarylayer,domaincat), (y0boundarylayer,y0cat), (0.0, 1.8e3), interfaces, (pboundarylayer,pcat,pinter));
+    @time sol = solve(react.ode,Sundials.CVODE_BDF(),abstol=1e-22,reltol=1e-8);
+    println(sol.t[end]);
+    println(sol.retcode);
+
+    # sol is the solution object
+    return sol
+end
+
 
 # %%
-phi = -(1.37 + 0.414);
-
-# %%
-# Reservoir is a 100 mL (100e-6 m^3) cell
-# Proton concentration is 10^-7 mol/L (10^-4 mol/m^3)
-# CO2 concentration is 0.01 mol/L (10 mol/m^3), saturation solubility ~0.03 mol/L
-# AVratio in experiments is 36 m^-1 but is measured by surface area/reservoir volume
-# Area of the electrode is therefore 3.6e1 m^-1 * 1e2 * 1e-6 m^3 = 3.6e-3 m^2 = 36 cm^2
-# Assume boundary layer thickness d_bl = 1 mm or 1e-3 m
-# Volume of the boundary layer V_bl = 3.6e-3 m^2 * 1e-3 m = 3.6e-6 m^3
-# Actual AVratio is therefore 3.6e-3 m^2 / 3.6e-6 m^3 = 1e3 m^-1 (reciprocal of d_bl)
-# Amount of sites is 2.943e-5 mol/m^2 * 3.6e-3 m^2 = 10.595e-8 mol
-
-# For earlier simulations, a 100x linear scale factor is applied,
-# so volume becomes 100e-6 m^3 * (1e2)^3 = 100 m^3,
-# electrode area becomes 3.6e-3 * (1e2) ^2 = 3.6e1 m^2,
-# AVratio becomes 3.6e1 m^2 / 1e2 m^3 = 0.36 m^-1
-# Volume of the boundary layer becomes 3.6e1 m^2 * 1e-3 m = 3.6e-2 m^3
-
-C_proton = 1e-7*1e3;
-C_co2 = 1e-2*1e3;
-C_default = 1e-12;
-V_res = 1e3;
-layer_thickness = 1e-5;
-AVratio = 36;
-A_surf = V_res*AVratio;
-V_bl = A_surf*layer_thickness;
-# V_bl = V_res;
-sites = sitedensity*A_surf;
-
-# The initial conditions for individual species are moles not concentration, so we need to multiply concentration by boundary layer volume
-initialcondsboundarylayer = Dict(["proton"=>C_proton*V_bl,
-                                  "CO2"=>C_co2*V_bl,
-                                #   "H2"=>C_default*10*V_bl,
-                                #   "O=CO"=>C_default*V_bl,
-                                  "V"=>V_bl,"T"=>300,"Phi"=>0.0,"d"=>0.0]);
-initialcondsreservoir = Dict(["proton"=>C_proton,
-                              "CO2"=>C_co2,
-                              "V"=>V_res,"T"=>300]);
-
-
-# Assume voltage is -1.0 V vs. R.H.E. which equates to -1.414 V vs. S.H.E. at pH=7
-initialcondssurf = Dict(["CO2X"=>0.4*sites,
-        # "CHO2X"=>0.1*sites,
-        # "CO2HX"=>0.1*sites,
-        # "OX"=>0.1*sites,
-        # "OCX"=>0.1*sites,
-        "vacantX"=>0.6*sites,
-        # "CH2O2X"=>0.05*sites,
-        # "CHOX"=>0.04*sites,
-        # "CH2OX"=>0.01*sites,
-        "A"=>A_surf,"T"=>300,"Phi"=>phi]);
-
-# %%
-domainboundarylayer, y0boundarylayer, pboundarylayer = ConstantTVDomain(phase=boundarylayer, initialconds=initialcondsboundarylayer);
-domaincat,y0cat,pcat = ConstantTAPhiDomain(phase=surf,
-    initialconds=initialcondssurf);
-
-# %%
-# Set proton diffusivity to a higher value than calculated from Stokes Einstein equation
-# The values are taken from DOI: 10.1039/C8SC01253A
-# Values calculated from MD is 1.015 A^2/ps, experimental values are 0.932 A^2/ps.
-# 1 A^2/ps = 1e-8 m^2/s
-domainboundarylayer.diffusivity[6] = 0.932e-8
-
-# %%
-inter,pinter = ReactiveInternalInterfaceConstantTPhi(domainboundarylayer,
-  domaincat,interfacerxns,298.15,A_surf);
-
-# %%
-# start with 1mm layer thickness
-diffusionlayer = ConstantReservoirDiffusion(domainboundarylayer, initialcondsreservoir, A_surf, layer_thickness);
-
-# %%
-interfaces = [inter, diffusionlayer];
-
-# %%
-@time react,y0,p = Reactor((domainboundarylayer,domaincat), (y0boundarylayer,y0cat), (0.0, 1.8e3), interfaces, (pboundarylayer,pcat,pinter));
-
-# %%
-@time sol = solve(react.ode,Sundials.CVODE_BDF(),abstol=1e-22,reltol=1e-8);
-println(sol.t[end]);
-println(sol.retcode);
+sol = run_co2_reduction_simulation([])
 
 # %%
 ssys = SystemSimulation(sol,(domainboundarylayer,domaincat,), interfaces,p);
